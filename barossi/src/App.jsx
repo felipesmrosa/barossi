ee4afaffe4b0e6a316dbc4ea091959a1a7609feb
Felipe Miranda <felipem04.r@gmail.com>
2024-07-08 11:30:14 -0300

    Teste de Login

--- barossi/src/App.jsx
+++ barossi/src/App.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./Componentes/Layout/Index";
 import { Dashboard } from "./Paginas/Dashboard/Index";
+import { Login } from "./Paginas/Home/Index";
 
 export function App() {
   return (
-    <>
-      <h1>Teste</h1>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+      <Route element={<Layout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+    </Routes>
   );
 }
