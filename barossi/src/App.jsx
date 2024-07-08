28abede3c3c2389a03853f5f654598644d6ee19e
Felipe Miranda <felipem04.r@gmail.com>
2024-07-08 11:25:03 -0300

    teste
    teste

--- barossi/src/App.jsx
+++ barossi/src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { Route, Routes } from "react-router-dom"
-import { Layout } from "./Componentes/Layout/Index"
-import { Dashboard } from "./Paginas/Dashboard/Index"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./Componentes/Layout/Index";
+import { Dashboard } from "./Paginas/Dashboard/Index";
 
 export function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Route>
-    </Routes>
-  )
+    <>
+      <h1>Teste</h1>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
