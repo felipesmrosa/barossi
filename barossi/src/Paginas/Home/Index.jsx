f60ad0b947080b3674661eb598f1a6ac4e9073f6
Felipe Miranda <felipem04.r@gmail.com>
2024-07-08 11:38:28 -0300

    Botão de teste para Dashboard

--- barossi/src/Paginas/Home/Index.jsx
+++ barossi/src/Paginas/Home/Index.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
+  const navigate = useNavigate();
+
+  const logar = () => {
+    navigate('/dashboard');
+  };
+
   return (
     <div>
       <h1> Login </h1>
+      <button onClick={logar}>Dashboard</button>
     </div>
   );
 }
