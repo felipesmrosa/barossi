b1c0b0dff90582e879e976d31842fea4b4b15073
Felipe Miranda <felipem04.r@gmail.com>
2024-07-21 18:08:28 -0300

    Adicionar mais funcionabilidades para os inputs de cadastrar alunos.

--- barossi/src/Paginas/Alunos/CadastrarAluno/Index.jsx
+++ barossi/src/Paginas/Alunos/CadastrarAluno/Index.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import Select from "react-select";
+import InputMask from "react-input-mask";
 import { Titulo } from "@/Componentes/FundoPadrao/Titulo/Index";
+import { modalidades } from "@/utils/data";
+import { customStyles } from "@/utils/data";
+import { validarCPF } from "@/utils/validaCPF";
+import { NumberFormatBase } from "react-number-format";
+
+// validarCPF
 
 export function CadastroDeAlunos() {
+  const [cpf, setCpf] = useState("");
+  const [cpfError, setCpfError] = useState("");
+  const [selectedModalidades, setSelectedModalidades] = useState([]);
+  const [mensalidade, setMensalidade] = useState("");
+  const [desconto, setDesconto] = useState("");
+
+  const handleCpfChange = (event) => {
+    const newCpf = event.target.value;
+    setCpf(newCpf);
+
+    if (validarCPF(newCpf)) {
+      setCpfError("");
+    } else {
+      setCpfError("CPF inválido");
+    }
+  };
+
+  const handleModalidadeChange = (selectedOptions) => {
+    setSelectedModalidades(selectedOptions);
+
+    // Calcula a soma das mensalidades
+    const totalMensalidade = selectedOptions.reduce((total, option) => {
+      const modalidade = modalidades.find((m) => m.value === option.value);
+      return (
+        total +
+        (modalidade
+          ? parseFloat(
+              modalidade.mensalidade.replace(".", "").replace(",", ".")
+            )
+          : 0)
+      );
+    }, 0);
+
+    // Aplica o desconto
+    const descontoValor = desconto ? parseFloat(desconto) / 100 : 0;
+    const valorComDesconto = totalMensalidade * (1 - descontoValor);
+
+    // Atualiza o estado com o valor formatado
+    setMensalidade(valorComDesconto);
+  };
+
+  const handleDescontoChange = (event) => {
+    let newDesconto = event.target.value;
+
+    // Limita o desconto a 100%
+    if (parseFloat(newDesconto) > 100) {
+      newDesconto = "100";
+    }
+
+    setDesconto(newDesconto);
+
+    // Atualiza mensalidade com base no desconto
+    handleModalidadeChange(selectedModalidades);
+  };
+
   return (
     <div className="fundoPadrao">
       <Titulo voltarPagina={true} titulo={"Novo aluno"} botao={"Cadastrar"} />
@@ -22,27 +85,95 @@ export function CadastroDeAlunos() {
               <label id="required" htmlFor="">
                 CPF
               </label>
-              <input type="text" />
+              <InputMask
+                mask="999.999.999-99"
+                value={cpf}
+                onChange={handleCpfChange}
+              >
+                {(inputProps) => <input type="text" {...inputProps} />}
+              </InputMask>
+              {cpfError && <p style={{ color: "red" }}>{cpfError}</p>}
             </div>
             <div className="cardPadrao__card__formulario__input">
               <label id="required" htmlFor="">
-                Telefone
+                Whatsapp
               </label>
-              <input type="text" />
+              <InputMask mask="(99) 9 9999-9999" alwaysShowMask={false}>
+                {(inputProps) => <input type="text" {...inputProps} />}
+              </InputMask>
             </div>
             <div className="cardPadrao__card__formulario__input">
               <label id="required" htmlFor="">
                 Modalidade
               </label>
-              <input type="text" />
+              <Select
+                styles={customStyles}
+                isMulti
+                name="colors"
+                options={modalidades}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                placeholder="Selecione a(s) modalidade(s)"
+                onChange={handleModalidadeChange}
+              />
+            </div>
+            <div className="cardPadrao__card__formulario__input">
+              <label htmlFor="">Desconto (%)</label>
+              <input
+                type="number"
+                value={desconto}
+                onChange={handleDescontoChange}
+                min="0"
+                max="100"
+                step="0.01"
+              />
             </div>
             <div className="cardPadrao__card__formulario__input">
               <label id="required" htmlFor="">
                 Mensalidade
               </label>
-              <input type="text" />
+              <div
+                style={{ display: "flex", alignItems: "center", width: "100%" }}
+              >
+                <div
+                  style={{
+                    backgroundColor: "#cbcbcb",
+                    padding: "0 10px",
+                    height: "1.8rem",
+                    display: "flex",
+                    alignItems: "center",
+                    borderTopLeftRadius: "4px",
+                    borderBottomLeftRadius: "4px",
+                    color: "#f8a11e",
+                  }}
+                >
+                  <b>R$</b>
+                </div>
+                <NumberFormatBase
+                  value={mensalidade}
+                  thousandSeparator="."
+                  decimalSeparator=","
+                  prefix="R$ "
+                  displayType="text"
+                  renderText={(formattedValue) => (
+                    <input type="text" value={formattedValue} readOnly />
+                  )}
+                  style={{
+                    borderTopLeftRadius: "0",
+                    borderBottomLeftRadius: "0",
+                    borderLeft: "0",
+                    padding: "0 10px",
+                    boxSizing: "border-box",
+                    background: "transparent",
+                    cursor: "not-allowed",
+                  }}
+                />
+              </div>
             </div>
-            <div className="cardPadrao__card__formulario--titulo">
+            <div
+              id="separacao"
+              className="cardPadrao__card__formulario--titulo"
+            >
               <p>Endereço</p>
             </div>
             <div className="cardPadrao__card__formulario__input">
---
+++ barossi/src/utils/data.js
@@ -0,0 +1,61 @@
+export const customStyles = {
+  control: (provided) => ({
+    ...provided,
+    backgroundColor: "transparent",
+    border: "1px solid #borda-card", // Certifique-se de substituir $borda-card pelo valor hexadecimal ou pela variável correta
+    width: "100%",
+    height: "auto",
+    outline: "none",
+    fontSize: "16px",
+    boxShadow: "none",
+    "&:hover": {
+      borderColor: "#borda-card",
+    },
+  }),
+  valueContainer: (provided, state) => ({
+    ...provided,
+    padding: state.isMulti ? "0" : provided.padding,
+    paddingLeft: "10px",
+  }),
+  container: (provided) => ({
+    ...provided,
+    width: "100%",
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isSelected
+      ? "#0052CC"
+      : state.isFocused
+      ? "#E3E3E3"
+      : "white",
+    color: state.isSelected ? "white" : "black",
+    "&:hover": {
+      backgroundColor: "#E3E3E3",
+    },
+  }),
+  singleValue: (provided) => ({
+    ...provided,
+    color: "#0052CC",
+  }),
+  placeholder: (provided) => ({
+    ...provided,
+    color: "#666666",
+  }),
+  menu: (provided) => ({
+    ...provided,
+    marginTop: 0,
+  }),
+  menuList: (provided) => ({
+    ...provided,
+    padding: 0,
+  }),
+};
+
+export const modalidades = [
+  { value: "kungFu", label: "Kung Fu", mensalidade: "200,00" },
+  { value: "boxChines", label: "Box Chinês", mensalidade: "180,00" },
+  { value: "taekwondo", label: "Taekwondo", mensalidade: "220,00" },
+  { value: "pilates", label: "Pilates", mensalidade: "150,00" },
+  { value: "muayThai", label: "Muay Thai", mensalidade: "210,00" },
+  { value: "jiuJitsu", label: "Jiu-Jítsu", mensalidade: "230,00" },
+];
