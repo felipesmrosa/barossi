02683b7f71361051fc2430b0b9a52ab3142b1fe6
Felipe Miranda <felipem04.r@gmail.com>
2024-07-24 11:52:57 -0300

    Teste CSS

--- barossi/src/Componentes/Layout/Menu/Index.jsx
+++ barossi/src/Componentes/Layout/Menu/Index.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function Menu({ isOpen, abrirFechar }) {
   const navigate = useNavigate();
+  const [clickedItem, setClickedItem] = useState(null);
 
-  const redirecionador = (path) => {
+  const redirecionador = (path, index) => {
     abrirFechar();
     navigate(path);
+    setClickedItem(index);
+    setTimeout(() => setClickedItem(null), 300); // Remove a classe após a animação
   };
 
   return (
     <div className={`menu ${isOpen ? "open" : ""}`}>
       <ul>
-        <li onClick={() => redirecionador('/dashboard')}>Inicio</li>
-        <li onClick={() => redirecionador('/alunos')}>Alunos</li>
-        <li onClick={() => redirecionador('/alunos')}>Paga Ohana aí</li>
+        <li
+          onClick={() => redirecionador("/dashboard", 0)}
+          className={clickedItem === 0 ? "clicked" : ""}
+        >
+          Inicio
+        </li>
+        <li
+          onClick={() => redirecionador("/alunos", 1)}
+          className={clickedItem === 1 ? "clicked" : ""}
+        >
+          Alunos
+        </li>
+        <li
+          onClick={() => redirecionador("/alunos", 2)}
+          className={clickedItem === 2 ? "clicked" : ""}
+        >
+          Paga Ohana aí
+        </li>
       </ul>
     </div>
   );
