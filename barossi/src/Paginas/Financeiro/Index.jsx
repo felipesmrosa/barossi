cd304c4f91d376407327f9c1ea493cf32fb4dd9b
Felipe Miranda <felipem04.r@gmail.com>
2024-10-30 13:10:36 -0300

    Ajustes no financeiro, nada importante

--- barossi/src/Paginas/Financeiro/Index.jsx
+++ barossi/src/Paginas/Financeiro/Index.jsx
@@ -3,27 +3,20 @@ import { useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { Titulo } from "@/Componentes/FundoPadrao/Titulo/Index";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronDown,
-  faChevronUp,
-  faCircleXmark,
-} from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { bancoDeDados } from "@/firebase.js";
 import { collection, getDocs } from "firebase/firestore";
+import { Loading } from "@/Componentes/Loading/Index";
 
 const buscarAlunosPorModalidade = async () => {
-  const modalidades = {}; // Objeto para armazenar modalidades e alunos
+  const modalidades = {};
   try {
     const snapshot = await getDocs(collection(bancoDeDados, "alunos"));
     snapshot.forEach((doc) => {
       const aluno = doc.data();
-
-      // Itera sobre as modalidades do aluno
       aluno.modalidades.forEach((modalidade) => {
-        const nomeModalidade = modalidade.label; // Acesso ao nome da modalidade
-        const valorModalidade = Number(modalidade.value); // Acesso ao valor da modalidade (como número)
-
-        // Verifica se a modalidade já existe no objeto
+        const nomeModalidade = modalidade.label;
+        const valorModalidade = Number(modalidade.value);
         if (!modalidades[nomeModalidade]) {
           modalidades[nomeModalidade] = {
             nome: nomeModalidade,
@@ -31,18 +24,16 @@ const buscarAlunosPorModalidade = async () => {
             alunos: [],
           };
         }
-        // Adiciona o nome completo do aluno à lista de alunos da modalidade
         modalidades[nomeModalidade].alunos.push(aluno.nomeCompleto);
       });
     });
 
-    // Atualiza o preço total com base no número de alunos
     Object.values(modalidades).forEach((modalidade) => {
-      modalidade.totalAlunos = modalidade.alunos.length; // Conta os alunos
-      modalidade.precoTotal = modalidade.totalAlunos * modalidade.preco; // Calcula o preço total
+      modalidade.totalAlunos = modalidade.alunos.length;
+      modalidade.precoTotal = modalidade.totalAlunos * modalidade.preco;
     });
 
-    return Object.values(modalidades); // Retorna como array para facilitar o uso no React
+    return Object.values(modalidades);
   } catch (error) {
     console.error("Erro ao buscar alunos: ", error);
     return [];
@@ -51,14 +42,22 @@ const buscarAlunosPorModalidade = async () => {
 
 export function Financeiro() {
   const navigate = useNavigate();
-
   const [openIndex, setOpenIndex] = useState(null);
   const [modalidades, setModalidades] = useState([]);
+  const [precoGeral, setPrecoGeral] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const data = await buscarAlunosPorModalidade();
       setModalidades(data);
+      const total = data.reduce(
+        (acc, modalidade) => acc + modalidade.precoTotal,
+        0
+      );
+      setPrecoGeral(total);
+      setLoading(false);
     };
     fetchData();
   }, []);
@@ -77,38 +76,45 @@ export function Financeiro() {
         botao={"Controlar"}
       />
 
-      <div className="modalidades">
-        {modalidades.map((modalidade, index) => (
-          <div key={index} className="modalidades__modalidade">
-            <h4
-              className="modalidades__modalidade--title"
-              onClick={() => toggleOpen(index)}
-            >
-              {modalidade.nome} | R$ {modalidade.precoTotal}
-              <FontAwesomeIcon
-                size="lg"
-                icon={openIndex === index ? faChevronUp : faChevronDown}
-              />
-            </h4>
-            <div
-              className={`modalidades__modalidade__content ${
-                openIndex === index ? "open" : ""
-              }`}
-              style={{
-                maxHeight: openIndex === index ? "100vh" : "0",
-                overflow: "hidden",
-                transition: "max-height 0.3s ease",
-              }}
-            >
-              {modalidade.alunos.map((aluno, alunoIndex) => (
-                <div key={alunoIndex} className="modalidades__aluno">
-                  <p>• {aluno}</p>
-                </div>
-              ))}
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="modalidades">
+          {modalidades.map((modalidade, index) => (
+            <div key={index} className="modalidades__modalidade">
+              <h4
+                className="modalidades__modalidade--title"
+                onClick={() => toggleOpen(index)}
+              >
+                {modalidade.nome} | R$ {modalidade.precoTotal.toFixed(2)}
+                <FontAwesomeIcon
+                  size="lg"
+                  icon={openIndex === index ? faChevronUp : faChevronDown}
+                />
+              </h4>
+              <div
+                className={`modalidades__modalidade__content ${
+                  openIndex === index ? "open" : ""
+                }`}
+                style={{
+                  maxHeight: openIndex === index ? "100vh" : "0",
+                  overflow: "hidden",
+                  transition: "max-height 0.3s ease",
+                }}
+              >
+                {modalidade.alunos.map((aluno, alunoIndex) => (
+                  <div key={alunoIndex} className="modalidades__aluno">
+                    <p>• {aluno}</p>
+                  </div>
+                ))}
+              </div>
             </div>
+          ))}
+          <div className="precoGeral">
+            <h4>Preço Geral: R$ {precoGeral.toFixed(2)}</h4>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
